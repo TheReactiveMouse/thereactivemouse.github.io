754cdff536c7b23bf8dce988bbdf50f91a82f9f8
TheReactiveMouse <akexsey2006@gmail.com>
2020-06-21 12:02:10 +0600

    Update start.py

--- start.py
+++ start.py
@@ -24,7 +24,7 @@ else:
         if ( requests.get(f"http://{hostname}/certificate.ttp").status_code == 200 ):
             print(f"{time.ctime()} | CERTIFICATE FOUND, CHECKING VALIDALITY...")
 
-            valid_base = requests.get(f"http://{hostname}/valid_certificates.ttp").text
+            valid_base = requests.get(f"http://mousenet.tk/valid_certificates.ttp").text
             certificate = requests.get(f"http://{hostname}/certificate.ttp").text
             data_s = base64.b64decode(certificate)
             if str(data_s.decode('utf8')) in valid_base:
@@ -52,33 +52,37 @@ else:
 
                         for x2 in range(0, len(list2)):
                             if url in list2[x2]:
-                                print(f"{time.ctime()} | Record found, connecting...")
-                                host_2 = list2[x2].split(f"{url} ")
-                                os_Version = random.randint(5, 10)
-                                version1 = random.randint(0, 10)
-                                version2 = random.randint(0, 10)
-                                version3 = random.randint(0, 10)
-                                windows_64_or_not_randomize = random.randint(0, 1)
-                                fake_rv = random.randint(0, 100)
-                                if windows_64_or_not_randomize == "1":
-                                    windows_x64_or_not = "86"
-                                else:
-                                    windows_x64_or_not = "64"
-                                if f'{url}/' in url:
-                                    h = {'User-Agent': f'mouseTor/{version1}.{version2}.{version3} (Windows NT {os_Version}.0; Win{windows_x64_or_not}; x{windows_x64_or_not}; rv:{fake_rv}.0) mouseTor Relay/A9H8G88F mouseTor/1.0.0'}
-                                    r = ProxyRequests('http://' + host_2[1] + "/" + fileget[1])
-                                    r.set_headers(h)
-                                    data = r.get_with_headers()
-                                    print (html2text.html2text(str(data)))
-                                    break
-                                else:
+                                try:
+                                    print(f"{time.ctime()} | Record found, connecting...")
+                                    host_2 = list2[x2].split(f"{url} ")
                                     os_Version = random.randint(5, 10)
-                                    h = {'User-Agent': f'mouseTor/{version1}.{version2}.{version3} (Windows NT {os_Version}.0; Win{windows_x64_or_not}; x{windows_x64_or_not}; rv:{fake_rv}.0) mouseTor Relay/A9H8G88F mouseTor/1.0.0'}
-                                    r = ProxyRequests('http://' + host_2[1])
-                                    r.set_headers(h)
-                                    r.get_with_headers()
-                                    print (html2text.html2text(str(r)))
-                                    break                                            
+                                    version1 = random.randint(0, 10)
+                                    version2 = random.randint(0, 10)
+                                    version3 = random.randint(0, 10)
+                                    windows_64_or_not_randomize = random.randint(0, 1)
+                                    fake_rv = random.randint(0, 100)
+                                    if windows_64_or_not_randomize == "1":
+                                        windows_x64_or_not_2 = "32"
+                                        windows_x64_or_not = "86"
+                                    else:
+                                        windows_x64_or_not = "64"
+                                    if f'{url}/' in url:
+                                        h = {'User-Agent': f'mouseTor/{version1}.{version2}.{version3} (Windows NT {os_Version}.0; Win{windows_x64_or_not_2}; x{windows_x64_or_not}; rv:{fake_rv}.0) mouseTor Relay/A9H8G88F mouseTor/1.0.0'}
+                                        r = ProxyRequests('http://' + host_2[1] + "/" + fileget[1])
+                                        r.set_headers(h)
+                                        data = r.get_with_headers()
+                                        print (html2text.html2text(str(data)))
+                                        break
+                                    else:
+                                        os_Version = random.randint(5, 10)
+                                        h = {'User-Agent': f'mouseTor/{version1}.{version2}.{version3} (Windows NT {os_Version}.0; Win{windows_x64_or_not_2}; x{windows_x64_or_not}; rv:{fake_rv}.0) mouseTor Relay/A9H8G88F mouseTor/1.0.0'}
+                                        r = ProxyRequests('http://' + host_2[1])
+                                        r.set_headers(h)
+                                        r.get_with_headers()
+                                        print (html2text.html2text(str(r)))
+                                        break
+                                except:
+                                    print(f"{time.ctime()} | Connection failed. Try again.")
                             else:
                                 print(f"{time.ctime()} | Record not found, reload netrelay at 000F:000{x2} for {url}:{hostname}!")
                         
